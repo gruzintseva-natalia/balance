6471af4f58781778553492c8a4bba197df3ff090
Gruzintseva Natalia <gruzintseva.natalia@gmail.com>
2022-05-19 07:04:29 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 200;
-        int payment = 1150;
+        int balance = 100;
+        int payment = 10;
         int bonusCost = 100;
         int bonus = payment / bonusCost;
-        int newBalance = balance + payment + bonus;
+        int newBalance = balance + payment;
         int minPaymentBonus = 1000;
+        boolean isAvailableBonus = payment >= minPaymentBonus;
 
-        if (payment >= minPaymentBonus) {
+        if (isAvailableBonus) {
             System.out.println("Вам начислен бонус " + bonus + " рублей") ;
+            newBalance += bonus;
         } else {
             System.out.println("Для получения бонуса добавьте еще чуть-чуть");
         }
